008c6cf1e5d06026c0f4439de7b36e4ec75e4ef6
Diego Castronuovo <castronu@gmail.com>
2015-01-18 20:54:41 +0100

    Merged

--- src/main/java/org/quickat/web/QuickieController.java
+++ src/main/java/org/quickat/web/QuickieController.java
@@ -14,6 +14,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,6 +43,9 @@ public class QuickieController {
 
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<FullQuickie> getQuickies(@RequestParam(value = "filter", defaultValue = "future", required = false) String filter) {
+        Authentication auth = SecurityContextHolder.getContext(). getAuthentication();
+        logger.info("GetQuickies request from user: {}",auth.getName());
+
         Iterable<Quickie> quickies = Collections.emptyList();
 
         switch (filter) {
