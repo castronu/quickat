9039244ad16b9a2306a7ef4369837a3820dc6d54
Christophe Pollet <cpollet@users.noreply.github.com>
2015-01-19 23:37:19 +0100

    removed useless $q from CountersController

--- src/main/webapp/js/CountersController.js
+++ src/main/webapp/js/CountersController.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
-    quickiesApp.controller('CountersController', ['$scope', '$rootScope', '$q', 'Quickies',
-        function ($scope, $rootScope, $q, Quickies) {
+    quickiesApp.controller('CountersController', ['$scope', '$rootScope', 'Quickies',
+        function ($scope, $rootScope, Quickies) {
             $scope.counters = Quickies.counters();
 
             $rootScope.$on('$routeChangeSuccess', function() {
