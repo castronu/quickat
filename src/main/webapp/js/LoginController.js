1baa1e6dbbb954474e92bed08c601acc8fd9fad0
Christophe Pollet <cpollet@users.noreply.github.com>
2015-01-21 21:06:00 +0100

    Better handling of login + display profile picture

--- src/main/webapp/js/LoginController.js
+++ src/main/webapp/js/LoginController.js
@@ -2,14 +2,14 @@
     'use strict';
     quickiesApp.controller('LoginController', ['$scope', 'store', '$location', 'auth',
         function ($scope, store, $location, auth) {
+            $scope.auth = auth;
+
             $scope.login = function () {
                 auth.signin({}, function (profile, token) {
                     // Success callback
                     store.set('profile', profile);
                     store.set('token', token);
                     $location.path('/');
-
-                    $scope.isAuthenticated = auth.isAuthenticated;
                 }, function () {
                     // Error callback
                 });
@@ -19,8 +19,6 @@
                 auth.signout();
                 store.remove('profile');
                 store.remove('token');
-
-                $scope.isAuthenticated = auth.isAuthenticated;
             };
 
 
